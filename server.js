c352ecce1a7cf400b328c3e13b7083869dc479c0
ispoljari <ivanspoljaric22@gmail.com>
2019-05-28 14:13:27 +0200

    Create server.js and start script

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+const board = {
+  lists: [
+    {
+      title: 'Example list 1',
+      cards: [
+        {
+          text: 'Example card 1'
+        },
+        {
+          text: 'Example card 2'
+        }
+      ]
+    },
+    {
+      title: 'Example list 2',
+      cards: [
+        {
+          text: 'Example card 1'
+        },
+        {
+          text: 'Example card 2'
+        }
+      ]
+    }
+  ]
+};
+
+app.get('/api/board', (req, res) => {
+  res.json(board);
+});
+
+app.listen(8080);
